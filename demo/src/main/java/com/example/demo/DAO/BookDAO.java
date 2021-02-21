11114d2b24d760e3352c143a5df6abb0a1ab325d
Parteek Saini <you@example.com>
2021-02-21 12:10:47 -0800

    First Commit

---
+++ demo/src/main/java/com/example/demo/DAO/BookDAO.java
@@ -0,0 +1,21 @@
+package com.example.demo.DAO;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.example.demo.Entity.Book;
+import java.util.Collection;
+
+@Component
+public class BookDAO {
+
+    @Autowired
+    private BookRepository repository;
+    public Collection<Book> getBooks(){
+        return repository.findAll();
+    }
+
+    public Book createBook(Book book){
+        return repository.insert(book);
+    }
+}
